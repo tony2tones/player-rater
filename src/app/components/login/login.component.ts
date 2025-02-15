646ba5478bed1e5e623bb532c9838c23020e708c
tony <anthonymarques@outlook.com>
2025-02-15 13:33:48 +0200

    login styles and button setup

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import {FormControl,ReactiveFormsModule, Validators} from '@angular/forms';
+import {  FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  signIn = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl(new FormControl(''), Validators.required),
+  });
+
 
 }
