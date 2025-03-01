c9985fae26caf548a55e09d1d3a56ac55cfd9f52
tony <anthonymarques@outlook.com>
2025-03-01 12:28:26 +0200

    added login with authgaurdand learned about circular dependencies and how to lazy load them

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { AppComponent } from './app.component';
+import { AuthGuardService } from './services/auth-guard.service';
 
 export const routes: Routes = [
   { path:'',
-    component: AppComponent
+    component: AppComponent,
+    canActivate: [AuthGuardService],
   },
 {
   path: 'auth/login',
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { inject, Injectable } from "@angular/core";
-import { Auth, createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile } from "@angular/fire/auth";
-import { from, Observable } from "rxjs";
+import { Auth, createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile, User } from "@angular/fire/auth";
+import { Router } from "@angular/router";
+import { from, Observable, of } from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -8,6 +9,7 @@ import { from, Observable } from "rxjs";
 
 export class AuthService {
   firebaseAuth = inject(Auth)
+  router = inject(Router)
 
   register(email:string, username:string, password:string): Observable<void> {
     const promise = createUserWithEmailAndPassword(this.firebaseAuth, email, password)
@@ -21,4 +23,24 @@ export class AuthService {
     const promise = signInWithEmailAndPassword(this.firebaseAuth, email, password).then(() => {})
     return from(promise)
   }
+
+  logout() {
+    this.firebaseAuth.signOut();
+    this.router.navigate(['/auth/login']);
+  }
+
+  isLoggedIn(): Observable<boolean> {
+    return new Observable((observer) => {
+      this.firebaseAuth.onAuthStateChanged((user) => {
+          observer.next(!!user);
+        observer.complete();
+      })
+    })
+  }
+
+  getCurrentUser():Observable<any> {
+    return of(this.firebaseAuth.currentUser);
+  }
+
+
 }
