e8cc8766384d669d22b4290622ae9e165435c3b8
tony <anthonymarques@outlook.com>
2025-03-01 14:49:58 +0200

    code clean up

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,6 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { AppComponent } from './app.component';
-import { authGuardGuard } from './services/auth-guard.guard';
 import { AuthGuardService } from './services/auth-guard.service';
 
 export const routes: Routes = [
