9f853fead62ecd9143b575149cf8deec2d469a46
tony <anthonymarques@outlook.com>
2025-03-12 12:31:53 +0200

    adding dropdowns got nothing to do with calling data :(

--- src/app/features/create-profile/create-profile.component.ts
+++ src/app/features/create-profile/create-profile.component.ts
@@ -4,10 +4,11 @@ import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { PlayerServiceService } from '../../services/player-service.service';
 import { CardComponent } from '../../components/card/card.component';
+import {NgSelectModule} from '@ng-select/ng-select';
 
 @Component({
   selector: 'app-create-profile',
-  imports: [ReactiveFormsModule, CardComponent],
+  imports: [ReactiveFormsModule, CardComponent, NgSelectModule],
   standalone: true,
   templateUrl: './create-profile.component.html',
   styleUrl: './create-profile.component.css'
@@ -19,6 +20,19 @@ fb = inject(FormBuilder);
 params = inject(ActivatedRoute);
 profileId: string | null = null;
 displayName: string | null = null;
+
+// dropdown values
+experienceOptions = [
+  { id: 1, name: 'Beginner' },
+  { id: 2, name: 'Intermediate' },
+  { id: 3, name: 'Advanced' }
+];
+
+roleOptions = [
+  { roleId: 1, roleName: 'Defender' },
+  { roleId: 2, roleName: 'Midfielder' },
+  { roleId: 3, roleName: 'Forward' }
+];
 
 constructor() {
   this.profileId = this.params.snapshot.paramMap.get('profileId');
